93f9b24be6716b2bee704b31fe5d7c4f013705b9
erdi20 <rayadavid158@gmail.com>
2025-04-05 21:54:28 +0700

    first commit

---
+++ src/components/Student.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    props: {
+        name: String
+    },
+    emits: ['response'],
+    methods: {
+        data() {
+            this.$emit('response', this.name)
+        }
+    }
+}
+</script>
+<template>
+    <slot>slot default</slot><br>
+    halo student {{ name }} <button @click="data">klik</button><br>
+</template>
